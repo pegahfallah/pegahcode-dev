93ff1e06ef053b106b5cb7bc1f676637763f6b30
Pegah <43738935+pegahfallah@users.noreply.github.com>
2023-12-29 05:08:46 -0500

    name

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -7,21 +7,21 @@ import { Card } from "../components/card";
 const socials = [
 	{
 		icon: <Twitter size={20} />,
-		href: "https://twitter.com/chronark_",
+		href: "https://twitter.com/pegah_",
 		label: "Twitter",
-		handle: "@chronark_",
+		handle: "@pegah_",
 	},
 	{
 		icon: <Mail size={20} />,
-		href: "mailto:dev@chronark.com",
+		href: "mailto:dev@pegah.com",
 		label: "Email",
-		handle: "dev@chronark.com",
+		handle: "dev@pegah.com",
 	},
 	{
 		icon: <Github size={20} />,
-		href: "https://github.com/chronark",
+		href: "https://github.com/pegah",
 		label: "Github",
-		handle: "chronark",
+		handle: "pegah",
 	},
 ];
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -6,19 +6,19 @@ import { Analytics } from "./components/analytics";
 
 export const metadata: Metadata = {
   title: {
-    default: "chronark.com",
-    template: "%s | chronark.com",
+    default: "pegah.com",
+    template: "%s | pegah.com",
   },
   description: "Co-founder of unkey.dev and founder of planetfall.io",
   openGraph: {
-    title: "chronark.com",
+    title: "pegah.com",
     description:
       "Co-founder of unkey.dev and founder of planetfall.io",
-    url: "https://chronark.com",
-    siteName: "chronark.com",
+    url: "https://pegah.com",
+    siteName: "pegah.com",
     images: [
       {
-        url: "https://chronark.com/og.png",
+        url: "https://pegah.com/og.png",
         width: 1920,
         height: 1080,
       },
@@ -38,7 +38,7 @@ export const metadata: Metadata = {
     },
   },
   twitter: {
-    title: "Chronark",
+    title: "pegah",
     card: "summary_large_image",
   },
   icons: {
