de91f2a2100bff06b31e858a3c908c7d705b0af1
Pegah <43738935+pegahfallah@users.noreply.github.com>
2024-03-12 04:34:39 -0400

    project descriptions

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -78,10 +78,10 @@ export default async function ProjectsPage() {
                 >
                   {featured.title}
                 </h2>
-                <p className="mt-4 leading-8 duration-150 text-zinc-400 group-hover:text-zinc-300">
+                <p className="mt-4 leading-8 duration-150 text-zinc-400 group-hover:text-zinc-300 mb-12">
                   {featured.description}
                 </p>
-                <div className="absolute bottom-4 md:bottom-8">
+                <div className="absolute bottom-4 md:bottom-8 ">
                   <p className="hidden text-zinc-200 hover:text-zinc-50 lg:block">
                     Read more <span aria-hidden="true">&rarr;</span>
                   </p>
